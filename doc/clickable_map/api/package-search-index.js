cf1f8614b576eb2cae749734d148a14ae499c804
H.Isota <isota@isota-mac.local>
2020-12-20 21:27:08 +0900

    MVN 対応

--- doc/clickable_map/api/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"isota.clickable_map","h":"","u":""},{"l":"isota.clickable_map.shape","h":"","u":""},{"l":"isota.test","h":"","u":""},{"l":"isota.util","h":"","u":""},{"l":"すべてのパッケージ","h":"","u":"allpackages-index.html"}]
